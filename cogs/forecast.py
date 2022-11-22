2f594320a6dc924c30fc28a3a3a0e0abe8f8128a
Usuariozombie <edupime_98@hotmail.com>
2022-11-22 18:38:43 +0100

    Update forecast.py

--- cogs/forecast.py
+++ cogs/forecast.py
@@ -19,7 +19,7 @@ class FORECAST(commands.Cog):
     
     @commands.command(help="🌡️ - It sends information about the selected location temperature.")
     async def weather(self ,ctx, *, location):
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={"7745a414566393bd03c26c9f7b65a359"}&units=metric'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={"yourApiID"}&units=metric'
         try:
             data = parse_data(json.loads(requests.get(url).content)['main'])
             data2 = parse_data(json.loads(requests.get(url).content)['weather'])
